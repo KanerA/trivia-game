512fe66f5163bf3cbb1701d95f4fef8289def4b9
Assaf Kaner <kaner.assaf@gmail.com>
2021-04-24 19:35:21 +0300

    add the question generator
    this function generates a question and answers according to the type.
    and sends out the object with question and answers attributes.
    doesn't check for the right answer

--- utils.js
+++ utils.js
@@ -3,4 +3,50 @@ const { country, question } = require('./models');
 const sequelize = require('sequelize');
 const { Sequelize } = require('sequelize');
 
-module.exports = {};
+const getQuestion = async (req, res) => {
+    const result = await question.findAll({
+        order: sequelize.literal('rand()'),
+        limit: 1,
+    });
+    const questionObject = {};
+    const selectedQuestion = result[0].dataValues;
+    questionObject.question = selectedQuestion.template;
+    let countries;
+    switch(selectedQuestion.type){
+        case 1:
+            countries = await country.findAll({
+                attributes: ['country', selectedQuestion.columns],
+                order: sequelize.literal('rand()'),
+                limit: 4,
+            });
+            questionObject.answers = countries.map(countryData => countryData.country);
+            res.json(questionObject);
+            break;
+        case 2:
+            countries = await country.findAll({
+                attributes: ['country', selectedQuestion.columns],
+                group: selectedQuestion.columns,
+                order: sequelize.literal('rand()'),
+                limit: 4,
+            });
+            let randomIndex = Math.floor(Math.random() * 4);
+            questionObject.question = questionObject.question.replace('~X~', countries[randomIndex].dataValues.country);
+            questionObject.answers = countries.map(countryData => countryData[selectedQuestion.columns]);
+            res.json(questionObject);
+            break;
+        case 3:
+            countries = await country.findAll({
+                attributes: ['country', selectedQuestion.columns],
+                order: sequelize.literal('rand()'),
+                limit: 2,
+            });
+            let randomIndex3 = Math.floor(Math.random() * 2);
+            questionObject.question = questionObject.question.replace('~X~', countries[randomIndex3].dataValues.country);
+            questionObject.question = questionObject.question.replace('~Y~', countries[1 - randomIndex3].dataValues.country);
+            questionObject.answers = ["true", "false"];
+            res.json(questionObject);
+            break;
+    }
+}
+
+module.exports = { getQuestion };
