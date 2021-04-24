cab3d1af0fadb585bb4c09d10152d88ee94d5398
Assaf Kaner <kaner.assaf@gmail.com>
2021-04-24 17:51:25 +0300

    fix question model

---
+++ migrations/20210424125137-create-question.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      template: {
+        type: Sequelize.STRING
+      },
+      columns: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.INTEGER
+      },
+      desc: {
+        type: Sequelize.BOOLEAN
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('questions');
+  }
+};
