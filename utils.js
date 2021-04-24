20a320b42af749d6ced302f0c6baae3f02bc9d6b
Assaf Kaner <kaner.assaf@gmail.com>
2021-04-24 19:33:44 +0300

    Update utils.js

---
+++ utils.js
@@ -0,0 +1,6 @@
+const { Op } = require('sequelize');
+const { country, question } = require('./models');
+const sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
+
+module.exports = {};
