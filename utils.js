1d5f92eef46aa0b28ba0aa64d6979caf9bf9b272
Assaf Kaner <kaner.assaf@gmail.com>
2021-04-25 14:24:21 +0300

    get the correct answer in type 2
    lines 53-59

--- utils.js
+++ utils.js
@@ -11,6 +11,7 @@ const getQuestion = async (req, res) => {
     questionObject.question = selectedQuestion.template;
     questionObject.desc = selectedQuestion.desc;
     let countries;
+    let answer;
     switch(selectedQuestion.type){
         case 1:
             countries = await country.findAll({
@@ -23,7 +24,7 @@ const getQuestion = async (req, res) => {
                 order: sequelize.literal('rand()'),
                 limit: 4,
             });
-            const answer = 
+            answer = 
                 (countries.sort((a, b) => {
                     if(selectedQuestion.desc) return b[selectedQuestion.columns] - a[selectedQuestion.columns];
                     return a[selectedQuestion.columns] - b[selectedQuestion.columns];
@@ -46,8 +47,16 @@ const getQuestion = async (req, res) => {
                 limit: 4,
             });
             let randomIndex = Math.floor(Math.random() * 4);
-            questionObject.question = questionObject.question.replace('~X~', countries[randomIndex].dataValues.country);
+            const replacementWord = countries[randomIndex].dataValues.country;
+            questionObject.question = questionObject.question.replace('~X~', replacementWord);
             questionObject.answers = countries.map(countryData => countryData.dataValues[selectedQuestion.columns]);
+            answer = 
+                (countries.filter(country => { // filter with the country in the question to get the right country object
+                    if(replacementWord === country.dataValues.country) return country.dataValues[selectedQuestion.columns];
+                })
+                )[0] // filter returns an array and we want the first and basically the only object in the array
+                .dataValues[selectedQuestion.columns]; // gets the value of the answer 
+            questionObject.answer = answer;
             res.json(questionObject);
             break;
         case 3:
