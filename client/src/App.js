618f74494e0770cf3cdecd248259b0cbb1e667d0
Assaf Kaner <kaner.assaf@gmail.com>
2021-04-25 17:28:03 +0300

    get question data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Question from './components/Question.js';
+import axios from 'axios';
 
 function App() {
-  let mockData = {
-    "question": "Which country is the largest by total area?",
-    "desc": true,
-    "answers": [
-    "Saint Vincent and the Grenadines",
-    "Ukraine",
-    "Tajikistan",
-    "Kyrgyzstan"
-    ]
-    }
+  const [questionData, setQuestionData] = useState([])
+
+  const getData = async () => {
+    const { data } = await axios.get('/quiz/question');
+    console.log(data);
+    setQuestionData([data]);
+  }
+
+  // let mockData = {
+  //   "question": "Which country is the largest by total area?",
+  //   "desc": true,
+  //   "answers": [
+  //   "Saint Vincent and the Grenadines",
+  //   "Ukraine",
+  //   "Tajikistan",
+  //   "Kyrgyzstan"
+  //   ]
+  //   }
+
+    useEffect(()=>{
+      getData()
+    }, []);
+    
   return (
     <div className="App">
-      <Question data={mockData} />
+      <Question data={questionData} />
     </div>
   );
 }
--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from "react";
 import Option from './Option.js';
-export default function Question(props) {
+
+export default function Question({ data }) {
+  console.log(data);
+  const questionData = data[0];
+  if(!data || data === []) return <div>
+    nothing
+  </div>;
 
   return (
-    // <div>
-    //   {props.data.answers.map(answer => <Option answer={answer} />)}
-
-    // </div>
-          <form action="/info" method="post">
-          <h1>{props.data.question}</h1>
-          {props.data.answers.map(answer =>
-          <div>
-          <input type="radio" id={answer} name="answer" value={answer} />
-          <label for={answer}>{answer}</label><br />
-          </div>
+          <form action="/quiz/question/rate" method="post">
+          <h1>{questionData.question}</h1>
+          {questionData.options&&questionData.options.map(option =>
+            <div>
+              <input type="radio" id={option} name="option" value={option} />
+              <label for={option}>{option}</label><br />
+            </div>
           )}
           <input type="submit" value="Submit"></input>
         </form>
