b69010413b7454bd767511ff620340dd8a7a03b4
David Bin Neun <davidbinneun@gmail.com>
2021-04-25 15:47:09 +0300

    form for question

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -3,9 +3,19 @@ import Option from './Option.js';
 export default function Question(props) {
 
   return (
-    <div>
-        <h1>{props.mockData.question}</h1>
-      {props.mockData.answers.map(answer => <Option answer={answer} />)}
-    </div>
+    // <div>
+    //   {props.data.answers.map(answer => <Option answer={answer} />)}
+
+    // </div>
+          <form action="/info" method="post">
+          <h1>{props.data.question}</h1>
+          {props.data.answers.map(answer =>
+          <div>
+          <input type="radio" id={answer} name="answer" value={answer} />
+          <label for={answer}>{answer}</label><br />
+          </div>
+          )}
+          <input type="submit" value="Submit"></input>
+        </form>
   );
 }
