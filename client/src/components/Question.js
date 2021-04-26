eb55bea38f7bb2d0abddb48fef7a471a15e0d09d
Assaf Kaner <kaner.assaf@gmail.com>
2021-04-26 10:25:53 +0300

    Update Question.js

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Option from './Option.js';
 
-export default function Question({ data }) {
-  console.log(data);
-  const questionData = data[0];
-  if(!data || data === []) return <div>
-    nothing
-  </div>;
+export default function Question({ question, options }) {
 
   return (
           <form action="/quiz/question/rate" method="post">
-          <h1>{questionData.question}</h1>
-          {questionData.options&&questionData.options.map(option =>
+          <h1>{question}</h1>
+          {options&&options.map(option =>
             <div>
               <input type="radio" id={option} name="option" value={option} />
-              <label for={option}>{option}</label><br />
+              <label htmlFor={option}>{option}</label><br />
             </div>
           )}
           <input type="submit" value="Submit"></input>
