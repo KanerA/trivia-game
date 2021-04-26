364d5682cffcc49bba483182efedcc6849c750f2
David Bin Neun <davidbinneun@gmail.com>
2021-04-26 23:00:50 +0300

    basic loop with game ending

--- client/src/App.js
+++ client/src/App.js
@@ -29,15 +29,10 @@ import axios from 'axios';
 
 export default function App() {
 	const [currentQuestion, setCurrentQuestion] = useState('');
-	const [questionAnswered, setQuestionsAnswered] = useState(0);
-	const [questionCount, setQuestionCount] = useState(1);
-
-	// const getData = async () => {
-	// 	const { data } = await axios.get('/quiz/question');
-	// 	console.log(data);
-	// 	setQuestion(data.question);
-	// 	setOptions(data.options)
-	// }
+	const [questionsAnswered, setQuestionsAnswered] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+	const [strikes, setStrikes] = useState(0);
 
 	useEffect(()=>{
 		getQuestion()
@@ -49,35 +44,29 @@ export default function App() {
 		console.log(data);
 	}
 
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
-
 	const handleAnswerOptionClick = (answerOption) => {
+		if (strikes === 2) setShowScore(true);
+
 		if (answerOption === currentQuestion.answer) {
 			setScore(score + 1);
-		}
+		} else setStrikes(strikes + 1);
 
-		setQuestionCount(questionCount + 1);
+		setQuestionsAnswered(questionsAnswered + 1);
 
 		getQuestion();
-		// const nextQuestion = currentQuestion + 1;
-		// if (nextQuestion < questions.length) {
-		// 	setCurrentQuestion(nextQuestion);
-		// } else {
-		// 	setShowScore(true);
-		// }
+		
 	};
 	return (
 		<div className='app'>
 			{showScore ? (
 				<div className='score-section'>
-					{/* You scored {score} out of {questions.length} */}
+					You scored {score} out of {questionsAnswered}
 				</div>
 			) : (
 				<>
 					<div className='question-section'>
 						<div className='question-count'>
-							<span>Question {questionCount}</span>
+							<span>Question {questionsAnswered + 1}</span>
 						</div>
 						<div className='question-text'>{currentQuestion.question}</div>
 					</div>
