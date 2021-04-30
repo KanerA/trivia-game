63d4f6f3090be9bc2f88342c280679e9fb540f4a
Assaf Kaner <kaner.assaf@gmail.com>
2021-04-30 18:26:59 +0300

    basic layout of login page
    also added on change functions to input areas

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-function Login(props) {
+function Login({onPasswordChange, onUserNameChange}) {
     return (
-        <div className = 'loginContainer'>
-            
+        <>
+            <div className = 'loginContainer'>
+                <div className = 'userName'>
+                    <label>Name:</label>
+                    <input onChange = {onUserNameChange} type = 'text' />
+                </div>
+                <div className = 'password'>
+                    <label>password</label>
+                    <input type = 'password' />
+                </div>
+                <button>Login</button>
+                <button>Sign-up</button>
+            </div>
             <Link to = '/quiz'>Quiz</Link>
-        </div>
+        </>
     );
 }
 
