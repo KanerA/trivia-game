eed73d995f428e64b4bab4f48e036f571f5bc3ac
Assaf Kaner <kaner.assaf@gmail.com>
2021-04-30 18:43:35 +0300

    basic sign up layout

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-function SignUp(props) {
+function SignUp({onClick, onUserNameChange, onPasswordChange}) {
     return (
-        <div>
-            
+        <div className = 'signUpContainer container'>
+            <div className = 'userName'>
+                <label>Name:</label>
+                <input onChange = {onUserNameChange} type = 'text' />
+            </div>
+            <div className = 'password'>
+                <label>password</label>
+                <input onChange = {onPasswordChange} type = 'password' />
+            </div>
+            <button onClick = {onClick}>Sign-up</button>
         </div>
     );
 }
