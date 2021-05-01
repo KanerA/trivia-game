573ae4cfe932a5d5a6747a605caf05db1a768683
Assaf Kaner <kaner.assaf@gmail.com>
2021-05-01 20:08:13 +0300

    Update index.js

--- routes/quiz/user/index.js
+++ routes/quiz/user/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { createUser, updateUserScore } = require('../../../utils');
+const { createUser, updateUserScore, userLogin } = require('../../../utils');
+const { validateToken } = require('../../../middelwares');
 const user = Router();
 
 user.post('/signup', createUser);
-user.post('/login', (Req,res) => {});
+user.post('/login', userLogin);
 user.patch('/', updateUserScore);
 
 module.exports = user;
