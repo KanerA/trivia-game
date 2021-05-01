0ae6031c06d64eae8056b5e143cdc7a9bc7d3174
Assaf Kaner <kaner.assaf@gmail.com>
2021-05-01 20:46:32 +0300

    create the rendering of the login comp
    the component will render out depends on the user's login, if error, it will show it and tell him to try again or sign up

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-function Login({onPasswordChange, onUserNameChange}) {
-    return (
-        <>
-            <div className = 'loginContainer'>
-                <div className = 'userName'>
-                    <label>Name:</label>
-                    <input onChange = {onUserNameChange} type = 'text' />
+function Login({onPasswordChange, onUserNameChange, onClick, loginError, setLoginError}) {
+    return ( loginError ? 
+            <div className = 'loginError'>
+                Name or Password are incorrect, please try again or sign up
+                <button onClick = {() => setLoginError(false)}>login</button>
+                <Link to = '/signup'><button>sign up</button></Link>
+            </div> 
+        :
+            <>
+                <div className = 'loginContainer'>
+                    <div className = 'userName'>
+                        <label>Name:</label>
+                        <input onChange = {onUserNameChange} type = 'text' />
+                    </div>
+                    <div className = 'password'>
+                        <label>password</label>
+                        <input onChange = {onPasswordChange} type = 'password' />
+                    </div>
+                    <button onClick = {onClick}>Login</button>
+                    <Link to ='/signup'><button>Sign-up</button></Link>
                 </div>
-                <div className = 'password'>
-                    <label>password</label>
-                    <input onChange = {onPasswordChange} type = 'password' />
-                </div>
-                <button>Login</button>
-                <Link to ='/signup'><button>Sign-up</button></Link>
-            </div>
-            <Link to = '/trivia'>Quiz</Link>
-        </>
+                <Link to = '/trivia'>Quiz</Link>
+            </>
     );
 }
 
