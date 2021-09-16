432936f41ae58d5fd1ba832f9fa4d169292093b5
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-16 20:33:38 +0300

    prettier

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -1,21 +1,30 @@
-import Option from './Option.js';
+import Option from "./Option.js";
 
-export default function Question({ currentQuestion, questionsAnswered, setUserAnswer }) {
-
-  return (<>
-        <div className='question-section'>
-          <div className='question-count'>
-            <span>Question {questionsAnswered + 1}</span>
-          </div>
-          <div className='question-text'>{currentQuestion.question}</div>
+export default function Question({
+  currentQuestion,
+  questionsAnswered,
+  setUserAnswer,
+}) {
+  return (
+    <>
+      <div className="question-section">
+        <div className="question-count">
+          <span>Question {questionsAnswered + 1}</span>
         </div>
-        <div className = 'answer-section'>
-          {
-            currentQuestion.options && currentQuestion.options.map((option, index) => (
-              <Option className='option' option = {option} index = {index} setUserAnswer = {setUserAnswer} />
-            ))
-          }
-        </div>
+        <div className="question-text">{currentQuestion.question}</div>
+      </div>
+      <div className="answer-section">
+        {currentQuestion.options &&
+          currentQuestion.options.map((option, index) => (
+            <Option
+              key={`option-${index}`}
+              className="option"
+              option={option}
+              index={index}
+              setUserAnswer={setUserAnswer}
+            />
+          ))}
+      </div>
     </>
   );
 }
--- client/src/components/RateQuestion.js
+++ client/src/components/RateQuestion.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import RateOption from './RateOption';
+import React from "react";
+import RateOption from "./RateOption";
 
-const rateOptions = [
-    1,2,3,4,5
-]
+const rateOptions = [1, 2, 3, 4, 5];
 
-function RateQuestion({onClick, userRating}) {
-    return (
-        <div className = 'rateQuestion'>
-            <div className = 'rateTitle'>
-                Rate the question!
-            </div>
-            <div className = 'rateOptions'>
-                {
-                    rateOptions.map(option => (
-                        <RateOption option = {option} userRating = {userRating} />
-                    ))
-                }
-            </div>
-            <div className = 'rateButtons'>
-                <button className = 'rateButton rate' onClick = {() => onClick(true)}>Rate!</button>
-                <button className = 'rateButton skip' onClick = {() => onClick(false)}>Skip</button>
-            </div>
-        </div>
-    );
+function RateQuestion({ onClick, userRating }) {
+  return (
+    <div className="rateQuestion">
+      <div className="rateTitle">Rate the question!</div>
+      <div className="rateOptions">
+        {rateOptions.map((option, index) => (
+          <RateOption
+            key={`option-${index}`}
+            option={option}
+            userRating={userRating}
+          />
+        ))}
+      </div>
+      <div className="rateButtons">
+        <button className="rateButton rate" onClick={() => onClick(true)}>
+          Rate!
+        </button>
+        <button className="rateButton skip" onClick={() => onClick(false)}>
+          Skip
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default RateQuestion;
