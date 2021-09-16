e9fad947c5688a61b830683bec08963c7cd97740
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-16 14:47:16 +0300

    fix save rated question
    the model was missing - underscored: true, so there wasn't a match with the fields

--- models/saved_question.js
+++ models/saved_question.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class saved_question extends Model {
     /**
@@ -12,19 +10,23 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  saved_question.init({
-    question: DataTypes.STRING,
-    option_1: DataTypes.STRING,
-    option_2: DataTypes.STRING,
-    option_3: DataTypes.STRING,
-    option_4: DataTypes.STRING,
-    answer: DataTypes.STRING,
-    rating: DataTypes.FLOAT,
-    vote_count: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'saved_question',
-  });
+  }
+  saved_question.init(
+    {
+      question: DataTypes.STRING,
+      option_1: DataTypes.STRING,
+      option_2: DataTypes.STRING,
+      option_3: DataTypes.STRING,
+      option_4: DataTypes.STRING,
+      answer: DataTypes.STRING,
+      rating: DataTypes.FLOAT,
+      vote_count: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "saved_question",
+      underscored: true,
+    }
+  );
   return saved_question;
 };
