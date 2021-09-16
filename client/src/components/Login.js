fe72addd935db13d62409851474fc70ac20d0eb0
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-16 16:04:30 +0300

    prettier

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,29 +1,39 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Login({onPasswordChange, onUserNameChange, onClick, loginError, setLoginError}) {
-    return ( loginError ? 
-            <div className = 'loginError'>
-                Name or Password are incorrect, please try again or sign up
-                <button onClick = {() => setLoginError(false)}>login</button>
-                <Link to = '/signup'><button>sign up</button></Link>
-            </div> 
-        :
-            <>
-                <div className = 'loginContainer'>
-                    <div className = 'userName'>
-                        <label>Name:</label>
-                        <input onChange = {onUserNameChange} type = 'text' />
-                    </div>
-                    <div className = 'password'>
-                        <label>password</label>
-                        <input onChange = {onPasswordChange} type = 'password' />
-                    </div>
-                    <button onClick = {onClick}>Login</button>
-                    <Link to ='/signup'><button>Sign-up</button></Link>
-                </div>
-            </>
-    );
+function Login({
+  onPasswordChange,
+  onUserNameChange,
+  onClick,
+  loginError,
+  setLoginError,
+}) {
+  return loginError ? (
+    <div className="loginError">
+      Name or Password are incorrect, please try again or sign up
+      <button onClick={() => setLoginError(false)}>login</button>
+      <Link to="/signup">
+        <button>sign up</button>
+      </Link>
+    </div>
+  ) : (
+    <>
+      <div className="loginContainer">
+        <div className="userName">
+          <label>Name:</label>
+          <input onChange={onUserNameChange} type="text" />
+        </div>
+        <div className="password">
+          <label>password:</label>
+          <input onChange={onPasswordChange} type="password" />
+        </div>
+        <button onClick={onClick}>Login</button>
+        <Link to="/signup">
+          <button>Sign-up</button>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Login;
