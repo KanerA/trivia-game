69267e06bee083943edbfbc469890d97c4c67d72
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-16 19:04:29 +0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,19 +2,17 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
-    password: process.env.SQL_DEV_PASSWORD,
-    database: process.env.SQL_DEV_DATABASE,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE,
     host: "127.0.0.1",
     dialect: "mysql",
     logging: false,
   },
   production: {
     username: "root",
-    password: "A$$af14",
-    database: "trivia",
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE,
+    host: "trivia-database",
     dialect: "mysql",
-    dialectOptions: {
-      socketPath: "/cloudsql/trivia-game-312813:europe-west3:cloud-sql",
-    },
   },
 };
