5b9c550374c36a798d3220cdc5684f115697f684
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-16 14:50:13 +0300

    prettier files

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,29 +1,53 @@
-import React from 'react';
-import Question from './Question';
-import RateQuestion from './RateQuestion';
-import ScoreBoard from './ScoreBoard';
-import UserScore from './UserScore';
+import React from "react";
+import Question from "./Question";
+import RateQuestion from "./RateQuestion";
+import ScoreBoard from "./ScoreBoard";
+import UserScore from "./UserScore";
 
-function Quiz({ questionsAnswered, correctAnswers, score,  currentQuestion, setUserAnswer, onClick, userRating, userAnswer, scoreBoard, players }) {
-    
-    return (
-        <div className='app'>
-			{scoreBoard ? (
-				<ScoreBoard players = {players} />
-			) : userAnswer ? 
-			<>
-				<UserScore score = {score} correctAnswers = {correctAnswers} questionsAnswered = {questionsAnswered} />
-				<Question currentQuestion = {currentQuestion} questionsAnswered = {questionsAnswered} setUserAnswer = {setUserAnswer} />
-				<RateQuestion onClick = {onClick} userRating = {userRating} />
-		 	</>
-			: (
-			<>
-				<UserScore score = {score} correctAnswers = {correctAnswers} questionsAnswered = {questionsAnswered} />
-				<Question currentQuestion = {currentQuestion} questionsAnswered = {questionsAnswered} setUserAnswer = {setUserAnswer} />
-			</>
-			)}
-		</div>
-    );
+function Quiz({
+  questionsAnswered,
+  correctAnswers,
+  score,
+  currentQuestion,
+  setUserAnswer,
+  onClick,
+  userRating,
+  userAnswer,
+  scoreBoard,
+  players,
+}) {
+  return (
+    <div className="app">
+      <ScoreBoard players={players} />) : userAnswer ?
+      <>
+        <UserScore
+          score={score}
+          correctAnswers={correctAnswers}
+          questionsAnswered={questionsAnswered}
+        />
+        <Question
+          currentQuestion={currentQuestion}
+          questionsAnswered={questionsAnswered}
+          setUserAnswer={setUserAnswer}
+        />
+        <RateQuestion onClick={onClick} userRating={userRating} />
+      </>
+      : (
+      <>
+        <UserScore
+          score={score}
+          correctAnswers={correctAnswers}
+          questionsAnswered={questionsAnswered}
+        />
+        <Question
+          currentQuestion={currentQuestion}
+          questionsAnswered={questionsAnswered}
+          setUserAnswer={setUserAnswer}
+        />
+      </>
+      )}
+    </div>
+  );
 }
 
 export default Quiz;
