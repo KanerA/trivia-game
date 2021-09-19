208d8da5a51fad21f3c0c2787a51435feea196ae
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-19 11:47:54 +0300

    prettier

--- client/src/components/ScoreBoard.js
+++ client/src/components/ScoreBoard.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ScoreBoardTitle from './ScoreBoardTitle';
-import ScoreBoardUser from './ScoreBoardUser';
+import React, { Link } from "react";
+import ScoreBoardTitle from "./ScoreBoardTitle";
+import ScoreBoardUser from "./ScoreBoardUser";
 
 function ScoreBoard({ players }) {
-    return (
-        <div className = 'scoreBoard'>
-            <ScoreBoardTitle />
-            {
-                players&&players.map((player, index) => (
-                    <ScoreBoardUser score = {player.score} name = {player.name} index = {index} />
-                ))
-            }
-        </div>
-    );
+  return (
+    <div className="scoreBoard">
+      <ScoreBoardTitle />
+      {players &&
+        players.map((player, index) => (
+          <ScoreBoardUser
+            score={player.score}
+            name={player.name}
+            index={index}
+          />
+        ))}
+    </div>
+  );
 }
 
 export default ScoreBoard;
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/quiz",
     createProxyMiddleware({
-      // target: 'http://localhost:3001',
+      // target: "http://localhost:3001",
       target: "http://trivia-server:3001", //for docker use
       changeOrigin: true,
     })
